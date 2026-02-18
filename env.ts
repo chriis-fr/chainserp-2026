4dee11567ac3827cbbff51909550cd19cc3685d4
chriis-fr <caspianodhis@gmail.com>
2026-02-18 11:42:40 +0300

    env vars

--- env.ts
+++ env.ts
@@ -1,6 +1,5 @@
 export const EnvVars = {
   SITE_NAME: 'Chains- ERP & Global Finance',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  URL: 'https://chains-erp.com',
   CALENDLY_URL: 'https://calendly.com/caspianodhis/30min',
 };
